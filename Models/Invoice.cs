3e8e075573fb8198db2e3a626c293b8eb5f4414d
Parth Dalvi <124716803+ParthDalvi164@users.noreply.github.com>
2025-08-19 13:13:59 +0530

    Update Invoice.cs

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -7,6 +7,7 @@ public class Invoice
     public int Id { get; set; }
     public DateTime OrderDateTime { get; set; }
     public double? TotalAmount { get; set; }
+    public double? AmountAfterDiscount { get; set; }
     public int OrderId { get; set; }
     public virtual Order? Order { get; set; }
     public int DeliveryAgentId { get; set; }
